95abfaa0bb65a509592719581c6efd649e5dcac8
adatasinfo <autodataxx@gmail.com>
2019-01-19 14:58:37 -0500

    update db logging

--- startup/db.js
+++ startup/db.js
@@ -1,16 +1,31 @@
 const mongoose = require('mongoose');
 const config = require("../config/default.json");
-const { LogMessage } = require('../middleware/error');
+const winston = require('winston');
+require('winston-mongodb');
 
 const mongoDbUrl = config.db;
 
 module.exports = function () {
+
+    const logger = winston.createLogger({
+        level: 'info',
+        format: winston.format.json(),
+        transports: [
+            new winston.transports.Console(),
+            new winston.transports.File({ filename: 'mongoDB.log' }),
+            new winston.transports.MongoDB({ db: config.db }) //log to mongoDB
+        ]
+    });
+
     ///username:password@host:port
     mongoose.connect(mongoDbUrl)
         .then(() => {
             var msg = 'Connected to MongoDB...' + mongoDbUrl;
-            LogMessage(msg, 'info', 'mongoDB.log');
-            // console.log(msg);
+            console.log(msg);
+        })
+        .catch(error => {
+            console.error('Could not connect to MongoDB', error);
+            logger.info(error.message, error);
         });
-        
+
 }
